a297934a5e9c3e5df0b39d76df46c57280e4331f
ellikarg <>
2023-06-25 11:52:26 +0000

    feat: add random numbers with javascript function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,23 @@ openRules.addEventListener('click', () => {
 closeRules.addEventListener('click', () => {
     rules.close();
 });
+
+/**
+ * Function that generates random numbers
+ * called when the script is first loaded
+ * and after the "Start Over"-button is clicked
+ */
+function randomNumbers() {
+    let num1 = Math.floor(Math.random() * 9) + 1;
+    let num2 = Math.floor(Math.random() * 9) + 1;
+    let num3 = Math.floor(Math.random() * 9) + 1;
+    let num4 = Math.floor(Math.random() * 9) + 1;
+
+    document.getElementById('random-number-1').innerHTML = num1;
+    document.getElementById('random-number-2').innerHTML = num2;
+    document.getElementById('random-number-3').innerHTML = num3;
+    document.getElementById('random-number-4').innerHTML = num4;
+
+}
+
+window.onload = randomNumbers();
