48aea2fea507051391137b69b95d11824144eca4
ellikarg <>
2023-06-27 10:10:42 +0000

    feat: add drag and drop function, with dummy text

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,71 @@ window.onload = randomNumbers();
 
 // Drag and Drop
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('dragstart', dragStart);
+    document.body.addEventListener('drop', drop);
+    document.body.addEventListener('dragover', dragOver);
+
+    document.body.addEventListener('mousedown', mouseDown);
+    document.body.addEventListener('dragenter', dragEnter);
+    document.body.addEventListener('dragleave', dragLeave);
+})
+
+function dragStart(event) {
+    let item = event.target;
+    if (!item.closest('.draggable')) return;
+    if (item.classList.contains('draggable')) {
+        item = item.firstElementChild;
+    }
+    console.log('DRAGSTART');
+    event.dataTransfer.setData('text/plain', 'some data');
+}
+
+function drop(event) {
+    let container = event.target;
+    if (!container.classList.contains('container')) return;
+
+    event.preventDefault();
+    console.log('DROP', event.dataTransfer);
+    let data = event.dataTransfer.getData('text/plain');
+    container.textContent += data;
+    container.classList.remove('over');
+}
+
+function dragOver(event) {
+    let container = event.target;
+    if (!container.classList.contains('container')) return;
+
+    event.preventDefault();
+    //container.classList.add('over');
+}
+
+function mouseDown(event) {
+    let item = event.target;
+    if (!item.closest('.draggable')) return;
+    item.style.cursor = 'grabbing';
+}
+
+function dragEnter(event) {
+    let container = event.target;
+    if (!container.classList.contains('container')) return;
+
+    event.preventDefault();
+    console.log('dragenter container');
+    container.classList.add('over');
+}
+
+function dragLeave(event) {
+    let container = event.target;
+    if (!container.classList.contains('container')) return;
+
+    event.preventDefault();
+    container.classList.remove('over');
+    console.log('dragleave container');
+}
+
+
+
 // Check Result and Start Over Buttons 
 
 document.getElementById('reset-button').addEventListener('click', resetFunction);
